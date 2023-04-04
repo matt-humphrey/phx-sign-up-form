db681febf581c46fcdbc0eb394308ec54a24f335
Matt Humphrey <mhumphrey9@hotmail.com>
2023-04-04 08:47:04 +0800

    initialize repo

---
+++ lib/sign_up_web/controllers/page_controller.ex
@@ -0,0 +1,19 @@
+defmodule SignUpWeb.PageController do
+  use SignUpWeb, :controller
+
+  def index(conn, _params) do
+    IO.inspect(conn.params)
+    render(conn, "index.html")
+  end
+
+  def submit(conn, _params) do
+    case User.new(conn.params) do
+      {:ok, data} ->
+        text(conn, "Welcome, #{data.username}!")
+      {:error, message} ->
+        text(conn, inspect(message.errors))
+    end
+  end
+
+  # defp error_message(
+end
